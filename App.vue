5368fe363a1801f7bcfa93b095b39e70cc6ba918
Jeff <sagalbot@gmail.com>
2019-11-29 16:04:29 -0800

    is this a bug?

---
+++ App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>
+      Hello <name #default="{person}">{{ person }}</name>
+    </h1>
+  </div>
+</template>
+
+<script>
+import Name from './Name'
+export default {
+  components: {Name},
+}
+</script>
---
+++ Name.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    <slot :person="person">{{ log() }}</slot>
+  </span>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    person: 'jeff',
+  }),
+  methods: {
+    /* eslint-disable */
+    log: () => console.trace(),
+  },
+};
+</script>
